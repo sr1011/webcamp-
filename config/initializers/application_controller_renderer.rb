e898dd22480702366f49e9b92aef08d2b0ee158c
sr1011 <ryo.s.1011.football@gmail.com>
2022-01-11 07:43:47 +0000

    画像投稿機能

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '91d78f05ada6dd03f4438b9a6318d1e333138efee87ada0f588cd5a4ec2438211b1e078382945a2e98e5eb3b206d13eb858b6c259142eb47de9f1ed048ccddbb'
